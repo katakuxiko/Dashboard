572dca0e40712764eaa6cc54518155c99d70b90f
Aleksei <katakuxiko@gmail.com>
2023-03-15 22:02:20 +0600

    stacked and sparkline

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,9 +1,28 @@
+import { ChartComponent, SeriesDirective, SeriesCollectionDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
 import React from 'react'
 
-const Stacked = () => {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy'
+
+const Stacked = ({width, height}) => {
   return (
-	<div>Stacked</div>
-  )
+		<ChartComponent
+			width={width}
+			height={height}
+			id="stack chart"
+			primaryXAxis={stackedPrimaryXAxis}
+			primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border:{width:0}}}
+      tooltip={{enable:true}}
+      legendSettings={{background:'white'}}
+		>
+			<Inject
+				services={[Legend, Category, StackingColumnSeries, Tooltip]}
+			/>
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item,i)=> <SeriesDirective key={i} {...item}/>)}
+      </SeriesCollectionDirective>
+		</ChartComponent>
+  );
 }
 
 export default Stacked
